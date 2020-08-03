cceb80d9b089de906aa13234ec159718b72da6d2
Samuel Santos <samuellima280499@gmail.com>
2020-08-03 00:46:25 -0300

    update gradient

--- src/components/PageDefault/style.js
+++ src/components/PageDefault/style.js
@@ -1,7 +1,7 @@
 import styled, { css } from "styled-components";
 
 export const Main = styled.main`
-  background-color: var(--black);
+  background-color: #141414;
   color: var(--white);
   flex: 1;
   padding: 80px 5% 0 5%;
